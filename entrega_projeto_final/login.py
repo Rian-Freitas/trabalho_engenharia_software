9aa12847a14a9b1b78685bb50b1b3ac5ca650f8f
Rian <rianfreitassilva@gmail.com>
2023-12-08 17:30:56 -0300

    Final interface

--- entrega_projeto_final/login.py
+++ entrega_projeto_final/login.py
@@ -1,111 +1,74 @@
 from tkinter import *
 from tkinter import messagebox
+from PIL import Image
+from PIL import ImageTk
 import sqlite3
 import os
 
 # Cria a janela de login
-class Login:
-    def __init__(self):
-        self.root = Tk()
-        self.root.title("Login")
-        self.root.geometry("925x500")
-        self.root.resizable(False, False)
-        self.root.configure(background="#f7f7f2")
-        self.root.iconbitmap(os.path.join(os.path.dirname(__file__), "figs\icone.ico"))
+root = Tk()
+root.title("Login")
+root.geometry("925x500+300+200")
+root.configure(background="#fff")
+root.resizable(False, False)
 
-        # Imagem da lateral
-        self.img = PhotoImage(file=os.path.join(os.path.dirname(__file__), r"figs\Nova_logomarca_do_Ecad.png")).subsample(4,4)
-        self.lbl_img = Label(self.root, image=self.img, bg="#f7f7f2")
-        self.lbl_img.place(x=-100, y=-70)
+def on_enter(e):
+    entry = e.widget
+    if entry.get() == "Login" or entry.get() == "Senha":
+        entry.delete(0, END)
+        entry.config(fg="#000")
 
-        # Conecta ao banco de dados
-        self.conn = sqlite3.connect("database.db")
-        self.cursor = self.conn.cursor()
+def on_leave(e):
+    entry = e.widget
+    if entry.get() == "":
+        entry.insert(0, "Login" if entry == user else "Senha")
+        entry.config(fg="#000")
 
-        # Cria os widgets
-        self.widgets()
+img_logo = ImageTk.PhotoImage(Image.open("entrega_projeto_final/figs/ecad_login.png").resize((150, 150), Image.LANCZOS))
+Label(root, image=img_logo, bg="#fff").place(x=-10, y=350)
 
-        # Inicia a janela
-        self.root.mainloop()
-    
-    # Cria os widgets
-    def widgets(self):
-        # Cria o frame
-        self.frame = Frame(self.root, bd=1, bg="#dde", highlightbackground="#999", highlightthickness=1)
-        self.frame.place(x=600, y=100)
+img_illustration = ImageTk.PhotoImage(Image.open("entrega_projeto_final/figs/illustration_login.png").resize((380, 380), Image.LANCZOS))
+Label(root, image=img_illustration, bg="#fff").place(x=480, y=40)
 
-        # Cria o label de usuário
-        self.lbl_usuario = Label(self.frame, text="Usuário:", bg="#dde", fg="#009", font=("Century Gothic", 12))
-        self.lbl_usuario.pack()
+frame = Frame(root, width=350, height=350, bg="#fff")
+frame.place(x=80, y=20)
 
-        # Cria a caixa de texto de usuário
-        self.txt_usuario = Entry(self.frame, font=("Century Gothic", 12))
-        self.txt_usuario.pack()
+heading = Label(root, text="Bem-vindo ao Ecad", fg="#57a1f8", bg="#fff", font=("Microsoft Yahei UI Light", 23, "bold"))
+heading.place(x=100, y=70)
 
-        # Cria o label de senha
-        self.lbl_senha = Label(self.frame, text="Senha:", bg="#dde", fg="#009", font=("Century Gothic", 12))
-        self.lbl_senha.pack()
+user = Entry(frame, width=25, bg="#fff", fg="#000", border=0, font=("Microsoft Yahei UI Light", 11))
+user.place(x=30, y=120)
+user.insert(0, "Login")
+user.bind("<FocusIn>", on_enter)
+user.bind("<FocusOut>", on_leave)
 
-        # Cria a caixa de texto de senha
-        self.txt_senha = Entry(self.frame, font=("Century Gothic", 12), show="*")
-        self.txt_senha.pack()
+password = Entry(frame, width=25, bg="#fff", fg="#000", border=0, font=("Microsoft Yahei UI Light", 11))
+password.place(x=30, y=180)
+password.insert(0, "Senha")
+password.bind("<FocusIn>", on_enter)
+password.bind("<FocusOut>", on_leave)
 
-        # Cria o botão de login
-        self.btn_login = Button(self.frame, text="Login", font=("Century Gothic", 12), command=self.login)
-        self.btn_login.pack(pady=10)
+Frame(frame, width=295, height=2, bg="#000").place(x=25, y=147)
+Frame(frame, width=295, height=2, bg="#000").place(x=25, y=207)
 
-        # Cria o botão de cadastro
-        self.btn_cadastro = Button(self.frame, text="Cadastro", font=("Century Gothic", 12), command=self.cadastro)
-        self.btn_cadastro.pack()
+option_list = ["Para artistas", "Para associações", "Para consumidores"]
+base_variable = StringVar()
+base_variable.set("Selecione o tipo de usuário")
 
-    # Função de login
-    def login(self):
-        # Pega os dados do usuário e senha
-        usuario = self.txt_usuario.get()
-        senha = self.txt_senha.get()
+option_menu = OptionMenu(frame, base_variable, *option_list)
+option_menu.config(width=25, bg="white", fg="black", border=1, font=("Microsoft Yahei UI Light", 11), activebackground="#a89157", activeforeground="#fff")
+option_menu.place(x=50, y=240)
 
-        # Verifica se os campos estão vazios
-        if (usuario == "" or senha == ""):
-            messagebox.showerror("Erro", "Preencha todos os campos!")
-        else:
-            # Verifica se o usuário existe
-            self.cursor.execute("SELECT * FROM usuarios WHERE usuario = ? AND senha = ?", (usuario, senha))
-            self.conn.commit()
-            usuario = self.cursor.fetchone()
+button = Button(frame, text="Entrar", width=10, bg="#fb6962", fg="#fff", border=0, font=("Microsoft Yahei UI Light", 11, "bold"), borderwidth=2, activebackground="#fff", activeforeground="#fb6962")
+button.place(x=120, y=310)
 
-            # Se o usuário existir, entra no sistema
-            if (usuario != None):
-                self.root.destroy()
-                # from home import Home
-                # Home()
-            else:
-                messagebox.showerror("Erro", "Usuário ou senha incorretos!")
+frame_sign_up = Frame(root, width=350, height=30, bg="#fff")
+frame_sign_up.place(x=480, y=420)
 
-    # Função de cadastro
-    def cadastro(self):
-        # Pega os dados do usuário e senha
-        usuario = self.txt_usuario.get()
-        senha = self.txt_senha.get()
+sign_up_text = Label(frame_sign_up, text="Não tem uma conta?", fg="#000", bg="#fff", font=("Microsoft Yahei UI Light", 11))
+sign_up_text.place(x=75, y=0)
 
-        # Verifica se os campos estão vazios
-        if (usuario == "" or senha == ""):
-            messagebox.showerror("Erro", "Preencha todos os campos!")
-        else:
-            # Verifica se o usuário existe
-            self.cursor.execute("SELECT * FROM usuarios WHERE usuario = ?", (usuario,))
-            self.conn.commit()
-            usuario = self.cursor.fetchone()
+sign_up_button = Button(frame_sign_up, text="Cadastre-se", width=10, bg="#fff", fg="#fb6962", border=0, font=("Microsoft Yahei UI Light", 10, "bold"), cursor="hand2")
+sign_up_button.place(x=220, y=-2)
 
-            # Se o usuário existir, mostra um erro
-            if (usuario != None):
-                messagebox.showerror("Erro", "Usuário já existe!")
-            else:
-                # Cadastra o usuário
-                self.cursor.execute("INSERT INTO usuarios (usuario, senha) VALUES (?, ?)", (usuario, senha))
-                self.conn.commit()
-                messagebox.showinfo("Sucesso", "Usuário cadastrado com sucesso!")
-                self.root.destroy()
-
-# Inicia o programa
-if (__name__ == "__main__"):
-    Login()    
+root.mainloop()
