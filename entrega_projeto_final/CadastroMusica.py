40c2e087ada410c0824d7aaa1bb7745496b3647b
Rodrigo Prieto <89661120+rdhery@users.noreply.github.com>
2023-12-10 20:15:04 -0300

    Modifique CadastroMusica.py

--- entrega_projeto_final/CadastroMusica.py
+++ entrega_projeto_final/CadastroMusica.py
@@ -1,24 +1,29 @@
 import sqlite3
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
+# Constante com o nome do banco de dados
 DB_FILE: str = "testbase2.db"
 
 
 # Classe Factory para criar instâncias do sistema
 class MusicSystemFactory:
+    """Factory para criar instâncias do sistema de música."""
     @staticmethod
     def create_music_system() -> 'MusicSystemFacade':
+        """Cria uma instância de MusicSystemFacade."""
         return MusicSystemFacade()
 
 
-# Classe Facade para fornecer uma interface para o sistema
+# Classe Facade para fornecer uma interface simplificada para o sistema
 class MusicSystemFacade:
+    """Fachada para interagir com o sistema de música."""
     def __init__(self):
         self.database: 'MusicDatabase' = MusicDatabase()
         self.certification_service: 'CertificationService' = CertificationService()
 
     def register_music(self, cod_artista: int, file_path: str) -> str:
+        """Registra uma nova música e emite um certificado se única."""
         composicao: str = self._read_file(file_path)
         if not composicao:
             return "Erro ao ler o arquivo"
@@ -31,6 +36,7 @@ class MusicSystemFacade:
             return "Esta música já foi registrada anteriormente. Não exclusiva."
 
     def _read_file(self, file_path: str) -> str:
+        """Lê o conteúdo de um arquivo."""
         try:
             with open(file_path, "r", encoding="utf-8") as file:
                 return file.read()
@@ -41,12 +47,14 @@ class MusicSystemFacade:
 
 # Classe para manipular o banco de dados
 class MusicDatabase:
+    """Classe para interagir com o banco de dados de músicas."""
     def __init__(self):
         self.connection: 'sqlite3.Connection' = sqlite3.connect(DB_FILE)
         self.cursor: 'sqlite3.Cursor' = self.connection.cursor()
         self._create_table()
 
     def _create_table(self) -> None:
+        """Cria a tabela 'obra' no banco de dados se não existir."""
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS obra (
@@ -58,23 +66,28 @@ class MusicDatabase:
         self.connection.commit()
 
     def check_uniqueness(self, composicao: str) -> bool:
+        """Verifica se uma composição já existe no banco de dados."""
         self.cursor.execute("SELECT * FROM obra WHERE content = ?", (composicao,))
         return not bool(self.cursor.fetchone())
 
     def register_music(self, composicao: str) -> None:
+        """Registra uma nova composição no banco de dados."""
         self.cursor.execute("INSERT INTO obra (content) VALUES (?)", (composicao,))
         self.connection.commit()
 
 
 # Classe para emitir certificados
 class CertificationService:
+    """Serviço para emitir certificados de propriedade intelectual."""
     def issue_certificate(self, cod_artista: int) -> str:
+        """Emite um certificado para um artista."""
         # Lógica para emitir certificado (pode ser personalizada conforme necessário)
         return f"Certificado de propriedade intelectual emitido para o músico {cod_artista}"
 
 
 # Interface Gráfica com Tkinter
 class MusicRegistrationGUI:
+    """Interface gráfica para o registro de músicas."""
     def __init__(self, master: tk.Tk):
         self.master: tk.Tk = master
         self.master.title("Registro de Música")
@@ -82,20 +95,23 @@ class MusicRegistrationGUI:
         self.create_widgets()
 
     def create_widgets(self) -> None:
+        """Cria os widgets da interface gráfica."""
         tk.Label(self.master, text="Selecione o arquivo de música:").pack(pady=10)
         tk.Button(self.master, text="Selecionar Arquivo", command=self.select_file).pack(pady=10)
         tk.Button(self.master, text="Registrar Música", command=self.register_music).pack(pady=10)
 
     def select_file(self) -> None:
+        """Abre uma caixa de diálogo para selecionar um arquivo."""
         file_path: str = filedialog.askopenfilename(filetypes=[("Arquivos de Texto", "*.txt")])
         self.file_path: str = file_path
         tk.Label(self.master, text=f"Arquivo Selecionado: {file_path}").pack()
 
     def register_music(self) -> None:
+        """Registra uma música."""
         cod_artista: int = 1  # Suponha que o ID do musicista seja 1 (pode ser obtido de forma dinâmica)
         system: 'MusicSystemFacade' = MusicSystemFactory.create_music_system()
         result_message: str = system.register_music(cod_artista, self.file_path)
-        tk.messagebox.showinfo("Resultado", result_message)
+        messagebox.showinfo("Resultado", result_message)
 
 
 if __name__ == "__main__":
